b6786ea5461fb07fd5fa39589d07e5625fab5d66
Tiger-WJ <113493414+Tiger-WJ@users.noreply.github.com>
2022-09-14 21:36:53 +0800

    Add files via upload

---
+++ if.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main1()
+{
+	int i=1 ;
+	/*while (i <= 100)
+	{
+		if (i % 2 != 0)
+			printf("%d ", i);
+			i = i++;
+
+	}*/
+	while (i < 100)
+	{
+		printf("%d ", i);
+		i = i++;
+	}
+		
+
+
+	
+	return 0;
+}
